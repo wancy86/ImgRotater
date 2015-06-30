199707fc0f4b44ba8a727281ed3fb41b4134f976
wancy86 <wancy86@sina.com>
2015-06-30 08:25:16 +0800

    aaa

--- rotater.js
+++ rotater.js
@@ -1,4 +1,40 @@
 
+//Desc
 //1.provide the img list, display area.
 //2.call init
+
+//Dependense
+//1. jQuery
+
+
+function ImgRotater()
+{
+	this.
+}
+
+ImgRotater.prototype.width=420;
+ImgRotater.prototype.height=250;
+
+ImgRotater.prototype.img_len=3;
+ImgRotater.prototype.width=420;
+
+ImgRotater.prototype.init=function(){
+	//crate the rotater element 
+	var target=this.target;
+
+}
+
+var rotater= new ImgRotater({
+	width: 420,
+	height: 250,
+	img_list: ['http://localhost:8888/QiuBoAi/images/rotater1.jpg',
+				'http://localhost:8888/QiuBoAi/images/rotater1.jpg',
+				'http://localhost:8888/QiuBoAi/images/rotater1.jpg',
+				'http://localhost:8888/QiuBoAi/images/rotater1.jpg'],
+	target: $('#top_rotater1')
+});
+
+rotater.init();
+
+
 
